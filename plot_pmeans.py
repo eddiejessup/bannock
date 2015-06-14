225cdf640b04b0be2b3cb75d58ab0ab81973c898
Elliot Marsden <elliot.marsden@gmail.com>
2015-06-14 01:55:49 +0100

    Add scripts to plot density stdev and mean tumble rate

---
+++ plot_pmeans.py
@@ -0,0 +1,16 @@
+from __future__ import print_function, division
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+import utils
+
+dirname = sys.argv[1]
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+
+ts, p_means, p_mins, p_maxs = utils.get_pmeans(dirname)
+plt.plot(ts, p_means)
+plt.plot(ts, p_mins)
+plt.plot(ts, p_maxs)
+plt.show()
