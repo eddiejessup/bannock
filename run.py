9c692dcd475bed3c559db680324ec067e736f3ae
Elliot Marsden <elliot.marsden@gmail.com>
2015-06-14 17:48:52 +0100

    Move util functions that run models to a new module

---
+++ run.py
@@ -0,0 +1,166 @@
+from __future__ import print_function, division
+import utils
+import model
+import runner
+import walls
+import cProfile
+import numpy as np
+
+default_wall_args = {
+    'L': 5000.0,
+    'dim': 2,
+    'dx': 40.0,
+}
+
+
+default_model_1d_kwargs = {
+    'seed': 1,
+    'dt': 0.2,
+    'v_0': 20.0,
+    'p_0': 1.0,
+    'L': default_wall_args['L'],
+    'dx': default_wall_args['dx'],
+    'c_D': 1000.0,
+    'c_sink': 0.01,
+    'c_source': 1.0,
+}
+
+
+default_model_kwargs = default_model_1d_kwargs.copy()
+del default_model_kwargs['L']
+del default_model_kwargs['dx']
+default_model_kwargs.update({
+    'D_rot': 0.2,
+    'force_mu': 0.0,
+})
+
+
+def run_model_1d(model_kwargs, output_dir, output_every, **iterate_args):
+    m = model.Model1D(**model_kwargs)
+    r = runner.Runner(output_dir, output_every, model=m)
+    r.iterate(**iterate_args)
+    return r
+
+
+def run_model(model_kwargs, walls, output_dir, output_every, **iterate_args):
+    m = model.Model(walls=walls, **model_kwargs)
+    r = runner.Runner(output_dir, output_every, model=m)
+    r.iterate(**iterate_args)
+    return r
+
+
+def iterate(m, n):
+    for _ in range(n):
+        m.iterate()
+
+
+def run_profile():
+    model_kwargs = default_model_kwargs.copy()
+    extra_model_kwargs = {
+        'rho_0': 2e-4,
+        'onesided_flag': True,
+        'chi': 10.0,
+    }
+    model_kwargs.update(extra_model_kwargs)
+
+    w = walls.Walls(**default_wall_args)
+    m = model.Model(walls=w, **model_kwargs)
+    cProfile.runctx('iterate(m, n)',
+                    locals={'m': m, 'n': 10000, 'iterate': iterate},
+                    globals={}, sort='tottime')
+
+
+def run_profile_1d():
+    model_kwargs = default_model_1d_kwargs.copy()
+    extra_model_kwargs = {
+        'rho_0': 0.1,
+        'onesided_flag': True,
+        'chi': 10.0,
+    }
+    model_kwargs.update(extra_model_kwargs)
+
+    m = model.Model1D(**model_kwargs)
+    cProfile.runctx('iterate(m, n)',
+                    locals={'m': m, 'n': int(1e5), 'iterate': iterate},
+                    globals={}, sort='tottime')
+
+
+def run_1d():
+    model_kwargs = default_model_1d_kwargs.copy()
+    extra_model_kwargs = {
+        'rho_0': 0.1,
+        'onesided_flag': False,
+        'chi': 0.0,
+    }
+    model_kwargs.update(extra_model_kwargs)
+
+    dirname = runner.make_output_dirname(model_kwargs)
+    print(runner.make_output_dirname(model_kwargs))
+    run_model_1d(model_kwargs, output_dir=dirname, output_every=200,
+                 t_upto=2e4)
+
+
+def run():
+    model_kwargs = default_model_kwargs.copy()
+    extra_model_kwargs = {
+        'rho_0': 2e-5,
+        'onesided_flag': False,
+        'chi': 0.0,
+    }
+    model_kwargs.update(extra_model_kwargs)
+    w = walls.Walls(**default_wall_args)
+
+    dirname = runner.make_output_dirname(model_kwargs)
+    print(runner.make_output_dirname(model_kwargs))
+    run_model(model_kwargs, w, output_dir=dirname, output_every=200,
+              t_upto=2e4)
+
+
+def run_chi_scan_1d():
+    model_kwargs = default_model_1d_kwargs.copy()
+    extra_model_kwargs = {
+        'rho_0': 0.1,
+        'onesided_flag': False,
+        'chi': 'all',
+    }
+    model_kwargs.update(extra_model_kwargs)
+    print(runner.make_output_dirname(model_kwargs))
+
+    chis = np.linspace(0.0, 40.0, 40)
+    dstds = []
+    for chi in chis:
+        model_kwargs['chi'] = chi
+        dirname = runner.make_output_dirname(model_kwargs)
+        run_model_1d(model_kwargs, output_dir=dirname, output_every=200,
+                     t_upto=2e4)
+        dstd = utils.recent_dstd(dirname, t_steady=10000.0)
+        dstds.append(dstd)
+        print(chi, dstd)
+
+
+def run_chi_hysteresis_1d():
+    ramp_kwargs = {
+        'ramp_chi_0': 0.0,
+        'ramp_chi_max': 10.0,
+        'ramp_dchi_dt': 5e-5,
+        'ramp_t_steady': 1e4,
+        'ramp_dt': 32e2,
+    }
+    extra_model_kwargs = {
+        'rho_0': 0.1,
+        'onesided_flag': False,
+        'chi': ramp_kwargs['ramp_chi_0'],
+    }
+    model_kwargs = default_model_1d_kwargs.copy()
+    model_kwargs.update(ramp_kwargs)
+    model_kwargs.update(extra_model_kwargs)
+
+    dirname = runner.make_output_dirname(model_kwargs)
+    print(dirname)
+    m = model.RampModel1D(**model_kwargs)
+    r = runner.Runner(output_dir=dirname, output_every=2000, model=m)
+
+    while r.model.chi >= 0.0:
+        r.iterate(n=1)
+        if r.is_snapshot_time():
+            print(r.model.chi, utils.density_std(r.model))
--- utils.py
+++ utils.py
@@ -1,10 +1,6 @@
 from __future__ import print_function, division
-import cProfile
 import numpy as np
-import model
-import runner
 from runner import filename_to_model, get_filenames
-import walls
 
 
 def dstd_func(d):
@@ -56,105 +52,6 @@ def get_pmeans(dirname):
         p_mins.append(m.p.min())
         p_maxs.append(m.p.max())
     return ts, p_means, p_mins, p_maxs
-
-
-def iterate(m, n):
-    [m.iterate() for _ in range(n)]
-
-
-def run_profile():
-    w = walls.Walls(L=1000.0, dim=2, dx=20.0)
-    m = model.Model(seed=1, dt=0.01,
-                    rho_0=1e-3, v_0=20.0, D_rot=0.2,
-                    p_0=1.0, chi=1e1, onesided_flag=True,
-                    force_mu=0.0,
-                    vicsek_R=0.0,
-                    walls=w,
-                    c_D=1000.0, c_sink=0.01, c_source=1.0)
-    cProfile.runctx('iterate(m, n)',
-                    locals={'m': m, 'n': 10000, 'iterate': iterate},
-                    globals={}, sort='tottime')
-
-
-def run_profile_1d():
-    m = model.Model1D(seed=1, dt=0.01,
-                      rho_0=1e0, v_0=20.0,
-                      p_0=1.0, chi=1e1, onesided_flag=True,
-                      L=1000.0, dx=20.0,
-                      c_D=1000.0, c_sink=0.01, c_source=1.0)
-    cProfile.runctx('iterate(m, n)',
-                    locals={'m': m, 'n': int(1e5), 'iterate': iterate},
-                    globals={}, sort='tottime')
-
-
-def chi_scan_1d():
-    kwargs = {
-        'seed': 1,
-        'dt': 0.2,
-        'rho_0': 0.1,
-        'v_0': 20.0,
-        'p_0': 1.0,
-        'chi': None,
-        'onesided_flag': False,
-        'L': 5000.0,
-        'dx': 40.0,
-        'c_D': 1000.0,
-        'c_sink': 0.01,
-        'c_source': 1.0,
-    }
-
-    kwargs['chi'] = 'all'
-    print(runner.make_output_dirname(kwargs))
-
-    chis = np.linspace(0.0, 40.0, 40)
-    dstds = []
-    for chi in chis:
-        kwargs['chi'] = chi
-        m = model.Model1D(**kwargs)
-        dirname = runner.make_output_dirname(kwargs)
-        r = runner.Runner(output_dir=dirname, output_every=200, model=m)
-        r.iterate(t_upto=2e4)
-
-        dstd = recent_dstd(dirname, t_steady=10000.0)
-        dstds.append(dstd)
-        print(chi, dstd)
-
-
-def chi_hysteresis_1d():
-    kwargs = {
-        'seed': 1,
-        'dt': 0.2,
-        'rho_0': 0.1,
-        'v_0': 20.0,
-        'p_0': 1.0,
-        'chi': 0.0,
-        'onesided_flag': False,
-        'L': 5000.0,
-        'dx': 40.0,
-        'c_D': 1000.0,
-        'c_sink': 0.01,
-        'c_source': 1.0,
-    }
-
-    ramp_kwargs = {
-        'ramp_chi_0': 2.0,
-        'ramp_chi_max': 3.0,
-        'ramp_dchi_dt': 5e-6,
-        'ramp_t_steady': 1e4,
-        'ramp_dt': 32e3,
-    }
-
-    kwargs.update(ramp_kwargs)
-
-    dirname = runner.make_output_dirname(kwargs)
-    print(dirname)
-    m = model.RampModel1D(**kwargs)
-    r = runner.Runner(output_dir=dirname, output_every=2000, model=m)
-
-    while True:
-        r.iterate(n=1)
-        if r.is_snapshot_time():
-            print(r.model.chi, density_std(r.model))
 
 
 def chi_dstd(dirnames):
