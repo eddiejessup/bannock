a982b5198ce88f8f9525ce9e88891a5701f6b3a0
Elliot Marsden <elliot.marsden@gmail.com>
2015-07-08 18:24:21 +0100

    Add docstring for steady-state time averaging of a measure.

--- utils.py
+++ utils.py
@@ -128,6 +128,10 @@ def chi_bcfs(dirnames, t_steady=None):
     ----------
     dirnames: list[str]
         Model output directory paths.
+    t_steady: None or float
+        Time to consider the model to be at steady-state.
+        The measure will be averaged over all later times.
+        `None` means just consider the latest time.
 
     Returns
     -------
@@ -145,6 +149,26 @@ def chi_bcfs(dirnames, t_steady=None):
 
 
 def get_average_measure(dirname, get_measure_func, t_steady=None):
+    """
+    Calculate a measure of a model in an output directory, averaged over
+    all times when the model is at steady-state.
+
+    Parameters
+    ----------
+    dirname: str
+        Output directory
+    get_measure_func: function
+        Function which takes a :class:`Model` instance as a single argument,
+        and returns the measure of interest.
+    t_steady: None or float
+        Time to consider the model to be at steady-state.
+        `None` means just consider the latest time.
+
+    Returns
+    -------
+    measure: various
+        Average measure.
+    """
     if t_steady is None:
         return get_measure_func(get_recent_model(dirname))
     else:
