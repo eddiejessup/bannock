a011fdb314f5b46edc7aee41241563294deca429
Elliot Marsden <elliot.marsden@gmail.com>
2015-07-12 15:53:10 +0100

    Change default names from args to kwargs to reflect type.

--- defaults.py
+++ defaults.py
@@ -1,26 +1,26 @@
 from __future__ import print_function, division
 import walls
 
-default_wall_args = {
+default_wall_kwargs = {
     'L': 2500.0,
     'dx': 40.0,
 }
 
-default_extra_blank_args = {
+default_extra_blank_kwargs = {
     'dim': 2,
 }
-default_blank_args = dict(default_wall_args, **default_extra_blank_args)
+default_blank_kwargs = dict(default_wall_kwargs, **default_extra_blank_kwargs)
 
-default_extra_trap_args = {
+default_extra_trap_kwargs = {
     'n': 1,
     'd': 40.0,
     'w': 280.0,
     's': 80.0,
 }
-default_trap_args = dict(default_wall_args, **default_extra_trap_args)
+default_trap_kwargs = dict(default_wall_kwargs, **default_extra_trap_kwargs)
 
-walls_blank = walls.Walls(**default_blank_args)
-walls_traps_1 = walls.Traps(**default_trap_args)
+walls_blank = walls.Walls(**default_blank_kwargs)
+walls_traps_1 = walls.Traps(**default_trap_kwargs)
 
 # Make 1d default model args
 default_model_1d_kwargs = {
@@ -30,8 +30,8 @@ default_model_1d_kwargs = {
     'p_0': 1.0,
     'origin_flag': False,
     'vicsek_R': 0.0,
-    'L': default_wall_args['L'],
-    'dx': default_wall_args['dx'],
+    'L': default_wall_kwargs['L'],
+    'dx': default_wall_kwargs['dx'],
     'c_D': 1000.0,
     'c_sink': 0.01,
     'c_source': 1.0,
