2dd1458e3f30435d23048f0a29b2bbd7ba6745cd
Elliot Marsden <elliot.marsden@gmail.com>
2015-08-22 23:31:05 +0100

    Bump version to 0.4.0

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ console_scripts = [
 
 setup(
     name='bannock',
-    version='0.3.1',
+    version='0.4.0',
     description="Agent-based simulations in confined environments",
     long_description=readme + '\n\n' + history,
     author="Elliot Marsden",
